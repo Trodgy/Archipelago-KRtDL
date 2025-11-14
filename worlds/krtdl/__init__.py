1d548abf94a964e009175c7345c868870d456aed
Trodgy <TrodgyPikachu@hotmail.com>
2025-11-14 00:30:21 +0000

    Update __init__.py

--- worlds/krtdl/__init__.py
+++ worlds/krtdl/__init__.py
@@ -126,7 +126,11 @@ class KRtDLWorld(World):
                 f.write(configjsons)
 
         # convert configjson to json
-        
+
+        options_dict: Dict[str, Union[int, str]] = {
+            #"progressive_beam_upgrades": self.options.progressive_beam_upgrades.value,
+            #"player_name": self.player_name,
+        }
         options_json = json.dumps(options_dict, indent=4)
         outfile_name = self.multiworld.get_out_file_name_base(self.player)
         krtdl = KRtDLContainer(configjsons, options_json, outfile_name, output_directory, player=self.player, player_name=self.player_name)
