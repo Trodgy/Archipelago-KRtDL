82cd4067c0b62a9e7f28c5ddeb9fb492f4305eec
Trodgy <TrodgyPikachu@hotmail.com>
2025-11-17 13:58:47 +0000

    Update __init__.py

--- worlds/krtdl/__init__.py
+++ worlds/krtdl/__init__.py
@@ -235,7 +235,7 @@ class KRtDLWorld(World):
         krtdl.write()
 
     def fill_slot_data(self) -> Mapping[str, Any]:
-        #print("test")
+        print("test")
         # If you need access to the player's chosen options on the client side, there is a helper for that.
         #return self.options.as_dict(
         #    "hard_mode", "hammer", "extra_starting_chest", "confetti_explosiveness", "player_sprite"
