af4457a2e9f65c9e2525061fed24d03645518055
Trodgy <TrodgyPikachu@hotmail.com>
2026-06-13 03:40:14 +0100

    Update KRtDLClient.py

--- worlds/krtdl/KRtDLClient.py
+++ worlds/krtdl/KRtDLClient.py
@@ -503,7 +503,7 @@ async def handle_checked_location(ctx: KRtDLContext, current_inventory: dict[str
     # ctx.dolphin_bridge.give_item_to_player(unknown_item1.id, 0, 0)
 
 async def handle_check_goal_complete(ctx: KRtDLContext):
-    if goal_type == 0 and state.has("Another Dimension Final Boss - Complete", world.player):
+    if ctx.goal_type == 0 and state.has("Another Dimension Final Boss - Complete", world.player):
         await ctx.send_msgs([{"cmd": "StatusUpdate", "status": ClientStatus.CLIENT_GOAL}])
     
     # if ctx.game_interface.current_game is not None:
