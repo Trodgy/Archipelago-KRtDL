911b16ffc3bbb48d50428636ebfbb949d10efcad
Trodgy <TrodgyPikachu@hotmail.com>
2026-06-15 02:28:27 +0100

    Update KRtDLClient.py

--- worlds/krtdl/KRtDLClient.py
+++ worlds/krtdl/KRtDLClient.py
@@ -500,9 +500,9 @@ async def handle_checked_location(ctx: KRtDLContext, current_inventory: dict[str
     # ctx.dolphin_bridge.give_item_to_player(unknown_item1.id, 0, 0)
 
 async def handle_check_goal_complete(ctx: KRtDLContext):
-    var hasmagolorgoal = false
-    var haslandiagoal = false
-    var hasdoomergoal = false
+    hasmagolorgoal: bool = false
+    haslandiagoal: bool = false
+    hasdoomergoal: bool = false
     for item in ctx.items_received:
         if item.item == 24102105:
             hasmagolorgoal = true
