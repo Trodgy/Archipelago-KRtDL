11a4969fc03434384f41bb9cb4e699388efeb8a0
Trodgy <TrodgyPikachu@hotmail.com>
2026-06-27 02:03:20 +0100

    Update Logic.py

--- worlds/krtdl/Logic.py
+++ worlds/krtdl/Logic.py
@@ -13,6 +13,8 @@ if TYPE_CHECKING:
 WorldOrderDefinition = [0, 1, 2, 3, 4, 5, 6] #Cookie Country to Dangerous Dinner
 WorldOrderDefinitionEX = [0, 1, 2, 3, 4, 5, 6] #EX Cookie Country to EX Dangerous Dinner
 
+#need to make this function return stuff instead of defining the bits above ^
+#as if there are multiple slots with the game they end up inheriting the previous one's world shuffles which is not right
 def DefineWorldOrder(world: "KRtDLWorld") -> None:
     WorldsDef = WorldOrderDefinition
     print(WorldsDef)
