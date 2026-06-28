5f00b6023d291daa873ac56c448a88c4ccd4463d
Trodgy <TrodgyPikachu@hotmail.com>
2026-06-28 12:39:07 +0100

    Update Logic.py

--- worlds/krtdl/Logic.py
+++ worlds/krtdl/Logic.py
@@ -47,7 +47,7 @@ def DefineWorldOrder(world: "KRtDLWorld") -> None:
                 print(ShuffleableWorlds[RandomIndex])
                 WorldsDef[i] = ShuffleableWorlds[RandomIndex]
                 ShuffleableWorlds.pop(RandomIndex)
-            else
+            else:
                 print("there is no")
                 print(i)
                 print("in the thing")
