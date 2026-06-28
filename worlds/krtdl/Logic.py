26021b14628af127b8842f52eff71f68fd842fcf
Trodgy <TrodgyPikachu@hotmail.com>
2026-06-28 11:42:14 +0100

    Update Logic.py

--- worlds/krtdl/Logic.py
+++ worlds/krtdl/Logic.py
@@ -37,7 +37,7 @@ def DefineWorldOrder(world: "KRtDLWorld") -> None:
     if len(ShuffleableWorlds) > 1:
         for i in range(0,len(ShuffleableWorlds)-1):
             ArrayIndex = ShuffleableWorlds[0]
-            RandomIndex = world.random.randrange(0,len(ShuffleableWorlds)-1)
+            RandomIndex = world.random.randrange(0,len(ShuffleableWorlds))
             print(RandomIndex)
             print(ShuffleableWorlds[RandomIndex])
             WorldsDef[ArrayIndex] = ShuffleableWorlds[RandomIndex]
