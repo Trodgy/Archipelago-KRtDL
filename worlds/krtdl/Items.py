d53daf1f52dee113a80c5c2b5302ca08717d6ee1
Trodgy <TrodgyPikachu@hotmail.com>
2026-07-12 18:30:09 +0100

    Update Items.py

--- worlds/krtdl/Items.py
+++ worlds/krtdl/Items.py
@@ -186,6 +186,7 @@ def generate_item_pool(world: "KRtDLWorld") -> List[KRtDLItem]:
     # These are items that are only added if certain options are set
     items: List[KRtDLItem] = []
 
+    print(world.world_gen["WorldDef"])
     if world.world_gen["WorldsShuffled"] > 1:
         if world.world_gen["WorldDef"][0] != 0:
             items.append(world.create_item(ItemNames.cookie_country_unlock.value, ItemClassification.progression))
